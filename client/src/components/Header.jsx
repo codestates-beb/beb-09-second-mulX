85dfad07226908a80da53657e9e3e7e8f8d56900
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-07-05 10:46:25 +0900

    main

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import style from '../assets/css/Header.module.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={style.headerContainer}>
+        <div className={style.logo}>logo</div>
+        <div className={style.menu}>
+            <div>Post</div>
+            <div>NFT</div>
+            <div>Faucet</div>
+        </div>
+        <div className={style.auth}>
+            <div>Login</div>
+            <div>Join</div>
+        </div>
+    </div>
   )
 }
 
