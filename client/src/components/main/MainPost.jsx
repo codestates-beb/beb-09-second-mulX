2fe4ca6267762ce00787cd7f80ce9b5731a4fd40
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-07-06 13:21:49 +0900

    add notion

--- client/src/components/main/MainPost.jsx
+++ client/src/components/main/MainPost.jsx
@@ -8,6 +8,8 @@ import dumyImg3 from '../../assets/img/path-4353699_1280.jpg'
 import dumyImg4 from '../../assets/img/snow-6071475_1280.jpg'
 
 const MainPost = () => {
+  //@notion 메인에 나오는 최신 포스트 화면
+  
   const InfoArr = [[dumyImg1,"Blue sky and green mountains", "gokite227", "2023.07.06"],
                     [dumyImg2, "River and mountain with clear water", "sjlee80", " 2023.07.05"],
                     [dumyImg3, "There is a mountain at the end of the trail", "stcr96", "2023.07.04"],
@@ -15,6 +17,7 @@ const MainPost = () => {
   return (
     <div className={styles.PostContainer}>
     <h3 className={styles.PostTitle}>New Post</h3>
+    {/* @notion 배열에있는 post 불러오기 */}
     <div className={styles.Postimg}>
       {
         InfoArr.map((info, i) => {
@@ -23,14 +26,6 @@ const MainPost = () => {
           )
         })
       }
-        
-        {/* <PostImg img={dumyImg2} />
-        <PostImg img={dumyImg3} />
-        <PostImg img={dumyImg4} /> */}
-      {/* <img  src={dumyImg1}></img> */}
-      {/* <img  src={dumyImg2}></img>
-      <img  src={dumyImg3}></img>
-      <img  src={dumyImg4}></img> */}
     </div>
   </div>
   )
--- client/src/components/main/MainTitle.jsx
+++ client/src/components/main/MainTitle.jsx
@@ -4,11 +4,15 @@ import styles from '../../assets/css/MainTitle.module.css';
 import MainBgImg from '../../assets/img/mountains-55067_640.png'
 
 const MainTitle = () => {
+
+  //타이틀을 나타내는 페이지
   return (
     <div>
       <div className={styles.MainContainer} >
         <img className={styles.MainBgImg} src={MainBgImg}></img>
+        {/* 메인 배경이미지 */}
         <div className={styles.TitleContainer}>
+          {/*메인 타이플 설명*/}
           <h2 className={styles.MainTitle}>MulX</h2>
           <div className={styles.MainText}>등산하는 사람들을 위한 인센티브 기반 커뮤니티</div>
         </div>
