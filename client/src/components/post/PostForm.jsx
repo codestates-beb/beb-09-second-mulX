805898b201db689a301c86de46911d64de7634fb
san <stcr96@gmail.com>
2023-07-06 17:00:06 +0900

    need fastforward

---
+++ client/src/components/post/PostForm.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef } from 'react';
+import './postForm.css';
+
+const PostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 게시글 등록 처리 로직 작성
+    console.log('게시글 정보:', title, content, selectedImage);
+    // 필요한 API 호출 등을 수행할 수 있습니다.
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleFileClick = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <div className="post-form-container">
+      <form onSubmit={handleSubmit} className="post-form">
+        <h1>게시글 작성</h1>
+        <div className="form-field">
+          <label htmlFor="title">제목:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="content">본문:</label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <div className="form-field">
+          <button type="button" onClick={handleFileClick}>사진 선택하기</button>
+          <input
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={handleFileChange}
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+          />
+        </div>
+        <button type="submit">게시글 등록</button>
+      </form>
+    </div>
+  );
+}
+
+export default PostForm;
