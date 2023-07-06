10aa5fce2824ae2a93aca7797a6e4235ae83f530
sjlee80 <sjlee80@gmail.com>
2023-07-06 17:13:25 +0900

    Smart Contract hardhat 배포, 회원가입 지갑 생성 구현

---
+++ smartContract/scripts/deploy.js
@@ -0,0 +1,28 @@
+const { ethers } = require('hardhat');
+
+async function mulXToken() {
+  const MulX20 = await ethers.getContractFactory('MulX20');
+  const mulX20 = await MulX20.deploy('My MulX20', 'MULX');
+
+  console.log('MulX20 Token deployed to:', mulX20.target);
+}
+
+async function mulXNFT() {
+  const MulX721 = await ethers.getContractFactory('MulX721');
+  const mulX721 = await MulX721.deploy();
+
+  console.log('MulX721 deployed to:', mulX721.target);
+}
+
+async function deploy() {
+  await mulXToken();
+  await mulXNFT();
+
+  console.log('MulXToken and MulX721 deployed successfully!');
+  process.exit(0);
+}
+
+deploy().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
