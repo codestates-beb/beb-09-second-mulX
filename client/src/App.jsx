07d46292814a750c84aec0f9b1e37ff5d1506b31
san <stcr96@gmail.com>
2023-07-10 08:38:33 +0900

    conflict 해결

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,10 @@ import Main from './page/MainPage';
 import Login from './page/LoginPage';
 import Signup from './page/SignupPage';
 import PostForm from './page/PostFormPage';
-<<<<<<< HEAD
 import Post from './page/PostPage'
 import PostDetail from './page/PostDetailPage';
 
-=======
 import MyPage from './page/MyPagePage';
->>>>>>> page
 
 function App() {
   return (
@@ -24,11 +21,8 @@ function App() {
             <Route path ="/signup" element={<Signup />} />
             <Route path ="/post" element={<Post />} />
             <Route path ="/postform" element={<PostForm />} />
-<<<<<<< HEAD
             <Route path ="/postdetail" element={<PostDetail />} />
-=======
             <Route path ="/mypage" element={<MyPage />} />
->>>>>>> page
           </Routes>
       </div>
     </BrowserRouter>
