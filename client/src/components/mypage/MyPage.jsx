41157bab2eaeb86cb018d3e2ba40c78935bca132
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-07-12 14:09:56 +0900

    fix mypage

--- client/src/components/mypage/MyPage.jsx
+++ client/src/components/mypage/MyPage.jsx
@@ -8,6 +8,7 @@ import dumyImg1 from '../../assets/img/mountain-world-1495832_1280.jpg'
 import dumyImg2 from '../../assets/img/mountains-4467436_1280.jpg'
 import dumyImg3 from '../../assets/img/path-4353699_1280.jpg'
 import dumyImg4 from '../../assets/img/snow-6071475_1280.jpg'
+import NftImg from '../nft/NftImg';
 
 const MyPage = () => {
   const isLoggedIn = useSelector((state) => state.isLoggedIn)
@@ -20,7 +21,9 @@ const MyPage = () => {
   const InfoArr = [[dumyImg1,"Blue sky and green mountains", "gokite227", "2023.07.06"],
                     [dumyImg2, "River and mountain with clear water", "sjlee80", " 2023.07.05"],
                     [dumyImg3, "There is a mountain at the end of the trail", "stcr96", "2023.07.04"],
-                    [dumyImg4, "Snowy mountain wonders", "codex1928", "2023.07.03"]]
+                    [dumyImg4, "Snowy mountain wonders", "codex1928", "2023.07.03"],
+                    [dumyImg1,"Blue sky and green mountains", "gokite227", "2023.07.06"],
+                    [dumyImg2, "River and mountain with clear water", "sjlee80", " 2023.07.05"]]
 
 
   return (
@@ -29,15 +32,15 @@ const MyPage = () => {
         <div className="profile-picture">
             <img src={profileImg} alt="프로필 사진" />
         </div>
+      </div>
+      <div className="profile-section">
         <div className="profile-details">
-          <h2>{userNickname}</h2>
+          <h2 className="profile-Nicname">{userNickname}</h2>
           <div className="address-details">
             <div className="profile-wallet">
-              <p>walletAddress: {walletAddress}</p>
-            </div>
-            <div className="profile-token-count">
-              <span>토큰 개수: {tokenCount}</span>
-              <span>Eth : {ethCount}</span>
+              <div>walletAddress: {walletAddress}</div>
+              <div>토큰 개수: {tokenCount}</div>
+              <div>Eth : {ethCount}</div>
             </div>
           </div>
         </div>
@@ -45,7 +48,7 @@ const MyPage = () => {
       
       <div className="my-posts-section">
         <h2>my Post</h2>
-        <div className={styles.Postimg}>
+        <div className="imgGrid">
           {
             InfoArr.map((info, i) => {
               return(
@@ -57,11 +60,14 @@ const MyPage = () => {
       </div>
       <div className="my-nfts-section">
         <h2>my NFT</h2>
-        <div className={styles.NFTimg}>
-          <img src={dumyImg1} alt="mypost-1"></img>
-          <img src={dumyImg2} alt="mypost-2"></img>
-          <img src={dumyImg3} alt="mypost-3"></img>
-          <img src={dumyImg4} alt="mypost-4"></img>
+        <div className="imgGrid">
+        {
+            InfoArr.map((info, i) => {
+              return(
+                <NftImg PostInfo={info} key={i}/>
+              )
+            })
+          }
         </div>
       </div>
     </div>
