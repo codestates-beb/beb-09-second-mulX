c7afbc11e6c55ea3c281523dda5e355d13991331
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-07-13 19:24:09 +0900

    NFTimg

--- client/src/components/nft/NftImg.jsx
+++ client/src/components/nft/NftImg.jsx
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from 'react';
 import styles from '../../assets/css/NftImg.module.css';
 
 const NftImg = ({ PostInfo }) => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
   const [imgUrl, setImgUrl] = useState(null)
 
   useEffect(() => {
     fetch(PostInfo)
       .then(response => response.json())
       .then(jsonData => setData(jsonData));
-    setImgUrl(data.image.replace('ipfs://', 'https://ipfs.io/ipfs/'))
+    const url = data? data.image.replace('ipfs://', 'https://ipfs.io/ipfs/') : null
+    console.log(url)
+    console.log("image", imgUrl)
+    setImgUrl(url)
+    console.log(data)
   }, []);
 
   
@@ -17,9 +21,9 @@ const NftImg = ({ PostInfo }) => {
     <div className={styles.NftContainer}>
       <div className={styles.PostInfo}>
       
-        <span className={styles.write}>{data.name}</span>
-        <h3 className={styles.title}>{data.title}</h3>
-        <span className={styles.day}>{data.category}</span>
+        <span className={styles.write}>{data? data.name : null}</span>
+        <h3 className={styles.title}>{data? data.title : null}</h3>
+        <span className={styles.day}>{data? data.category : null}</span>
         
       </div>
       <img src={imgUrl} className={styles.NftImg} alt="Post Image" />
